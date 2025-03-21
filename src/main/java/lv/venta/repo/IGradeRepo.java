0a64d2a082653ca02f8a1b24b3aa117bee8d7600
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-21 12:12:52 +0200

    Edit: added filtering functins in repos

--- src/main/java/lv/venta/repo/IGradeRepo.java
+++ src/main/java/lv/venta/repo/IGradeRepo.java
@@ -1,9 +1,16 @@
 package lv.venta.repo;
 
+import java.util.ArrayList;
+
 import org.springframework.data.repository.CrudRepository;
 
 import lv.venta.model.Grade;
 
 public interface IGradeRepo extends CrudRepository<Grade, Long>{
+
+	//izveidos šādu SQL vaicājumu:
+	//SELECT * FROM grade_table WHERE st_id = ?1;
+	//?1 -> pirmais funkcijas parametrs
+	public abstract ArrayList<Grade> findByStudentStId(long id);
 
 }
---
+++ src/main/java/lv/venta/service/impl/SchoolFilteringServiceImpl.java
@@ -0,0 +1,82 @@
+package lv.venta.service.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.model.Course;
+import lv.venta.model.Grade;
+import lv.venta.repo.ICourseRepo;
+import lv.venta.repo.IGradeRepo;
+import lv.venta.repo.IStudentRepo;
+import lv.venta.service.ISchoolFilteringService;
+
+@Service
+public class SchoolFilteringServiceImpl implements ISchoolFilteringService {
+
+	@Autowired
+	private IStudentRepo studRepo;
+	
+	@Autowired
+	private IGradeRepo grRepo;
+	
+	@Autowired
+	private ICourseRepo coRepo;
+	
+	@Override
+	public ArrayList<Grade> selectGradesByStudentId(long id) throws Exception {
+		if( id <= 0)
+		{
+			throw new Exception("Id should be positive");
+		}
+		
+		if(!studRepo.existsById(id))
+		{
+			throw new Exception("Student with id " + id + " doesn't exist");
+		}
+		ArrayList<Grade> result = grRepo.findByStudentStId(id);
+		
+		if(result.isEmpty())
+		{
+			throw new Exception("There is no grade linked to student");
+		}
+		
+		return result;
+	}
+
+	@Override
+	public ArrayList<Course> selectCoursesByStudentId(long id) throws Exception {
+		if( id <= 0)
+		{
+			throw new Exception("Id should be positive");
+		}
+		
+		if(!studRepo.existsById(id))
+		{
+			throw new Exception("Student with id " + id + " doesn't exist");
+		}
+		
+		ArrayList<Course> result = coRepo.findByGradesStudentStId(id);
+		
+		if(result.isEmpty())
+		{
+			throw new Exception("There is no course linked to student");
+		}
+		
+		return result;
+	}
+
+	@Override
+	public ArrayList<Course> selectCoursesByProfessorId(long id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float calculateAVGGradeInCourseId(long id) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
