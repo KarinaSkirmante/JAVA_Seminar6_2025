cfd61fa1696e10adb0f2add38a85a1e4525e0aaa
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-26 10:25:14 +0200

    Add: create filtering controller with function - get all grades for student

---
+++ src/main/java/lv/venta/controller/SchoolFilteringController.java
@@ -0,0 +1,38 @@
+package lv.venta.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.model.Grade;
+import lv.venta.service.ISchoolFilteringService;
+
+@Controller
+@RequestMapping("/school/filtering")
+public class SchoolFilteringController {
+	
+	@Autowired
+	private ISchoolFilteringService schoolFiltService;
+	
+	
+	@GetMapping("/grades/student/{id}")
+	public String getControllerGradesByStudent(@PathVariable(name = "id") long id, Model model)
+	{
+		try {
+			ArrayList<Grade> filteredGrades = schoolFiltService.selectGradesByStudentId(id);
+			model.addAttribute("package", filteredGrades);
+			return "show-grades";//parādīs show-grades.html lapu ar injencētu sarakstu no filteredGrades
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+	}
+	
+	
+
+}
