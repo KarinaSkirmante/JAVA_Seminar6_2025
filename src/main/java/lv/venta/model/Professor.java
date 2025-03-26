32d7ab5c5557ff252e926e91db23ea4fee5794d5
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-26 11:27:32 +0200

    Edit: changed to many-to-many linkage between professor and course

--- src/main/java/lv/venta/model/Professor.java
+++ src/main/java/lv/venta/model/Professor.java
@@ -1,10 +1,14 @@
 package lv.venta.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
@@ -43,10 +47,9 @@ public class Professor {
 	@Column(name = "Degree")//MYSQL - degree
 	private Degree degree;
 	
+	@ManyToMany(mappedBy = "professors")
 	@ToString.Exclude
-	@OneToOne(mappedBy = "professor")//otras klases mainīgaa noasukums, uz kuru ir JoinColumn
-	private Course course;
-	
+	private Collection<Course> courses = new ArrayList<>();
 	
 	
 	public Professor(String name, String surname, Degree degree) {
@@ -54,5 +57,14 @@ public class Professor {
 		setSurname(surname);
 		setDegree(degree);
 	}
+	
+	public void addCourse(Course course)
+	{
+		if(!courses.contains(course))
+		{
+			courses.add(course);
+		}
+	}
+	//TODO izveidot removeCourse
 	
 }
