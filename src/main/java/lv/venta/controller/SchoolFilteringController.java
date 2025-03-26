8cb60d9d17b6843f73d8e50da9241a1ef688ee20
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-26 11:08:12 +0200

    Add: create filtering controller with function - others get controller functions

--- src/main/java/lv/venta/controller/SchoolFilteringController.java
+++ src/main/java/lv/venta/controller/SchoolFilteringController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lv.venta.model.Course;
 import lv.venta.model.Grade;
+import lv.venta.model.Student;
 import lv.venta.service.ISchoolFilteringService;
 
 @Controller
@@ -48,6 +49,46 @@ public class SchoolFilteringController {
 		}
 	}
 
+	
+	@GetMapping("/courses/professor/{id}")//localhost:8080/school/filtering/courses/professor/1
+	public String getControllerCoursesByProfessor(@PathVariable(name = "id") long id, Model model)
+	{
+		try {
+			ArrayList<Course> filteredCourses = schoolFiltService.selectCoursesByProfessorId(id);
+			model.addAttribute("package", filteredCourses);
+			return "show-courses";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+	}
+	
+	
+	@GetMapping("/courses/avg/{id}")//localhost:8080/school/filtering/courses/avg/1
+	public String getControllerAVGGradeInCourse(@PathVariable(name = "id") long id, Model model)
+	{
+		try {
+			float avgGrade = schoolFiltService.calculateAVGGradeInCourseId(id);
+			model.addAttribute("avgrade", "AVG grade is: " + avgGrade);
+			return "show-avg-grade";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+	}
+	
+	@GetMapping("/student/failed")//localhost:8080/school/filtering/student/failed
+	public String getControllerFailedStudents(Model model)
+	{
+		try {
+			ArrayList<Student> filteredStudents = schoolFiltService.selectStudentsWithFailedGrades();
+			model.addAttribute("package", filteredStudents);
+			return "show-students";
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "show-error";
+		}
+	}
 	
 
 }
